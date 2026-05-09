23a49369ee3e5775d855b5e3e4dd572dab670685
iSurface <zraulcuenta@gmail.com>
2026-05-08 22:38:45 -0600

    Intento de Arreglo de Tabla de Semanticos

--- main.js
+++ main.js
@@ -101,8 +101,8 @@ async function analizar(){
 
     // Semantic
     pyodide.globals.set("ast", ast);
-    pyodide.globals.set("simbolos", simbolos);
-    let resultadoSemantic = pyodide.runPython(`analizador_semantico(ast, simbolos)`);
+    pyodide.globals.set("symbol_table", simbolos);
+    let resultadoSemantic = pyodide.runPython(`analizador_semantico(ast, symbol_table)`);
     console.log("Raw Pyodide result for semantic:", resultadoSemantic);
     
     // Simple conversion - let Pyodide handle it automatically
